87c041aadc4cb290039d5dc755a3711144ffb7a1
cws0316 <dev.chwonseok@gmail.com>
2021-11-08 18:30:11 +0900

    Add: default note for user

--- src/App.js
+++ src/App.js
@@ -2,8 +2,16 @@ import { useState } from 'react';
 import NotesList from './components/NotesList';
 import uniqid from 'uniqid';
 
+const defaultNote = [
+  {
+    id: uniqid(),
+    text: 'Default note\n\n\nthis is to show you how the note looks like when you add\nyou can delete this default note to start to use this App 😉\nHave fun!',
+    date: 'by chwonseok',
+  },
+];
+
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(defaultNote);
 
   const addNote = (note) => {
     const date = new Date().toLocaleDateString();
